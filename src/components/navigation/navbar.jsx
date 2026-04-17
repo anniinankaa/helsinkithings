22d84f3d782c67c59787afe709fbe452b4445f73
samujpg <samu.a.huttunen@aalto.fi>
2026-04-17 22:16:12 +0300

    mustakirjekuori lisätty + sivua korjailtu

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router"
+import './navbar.css'
 
 function Navbar () {
   return (
     <nav className="navbar">
-      <ul style={{listStyle: "none"}} className="navbar-list">
+      <img src="/navlogo.png" alt="musta kirjekuori"></img>
+      <ul className="navbar-list">
         <li className="navbar-item">
           <Link to="/about">About</Link>
         </li>
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,7 +2,6 @@ function HomePage () {
   return (
     <div>
       <h1>helsinkithings</h1>
-      <p>Tervetuloa</p>
     </div>
   )
 }
