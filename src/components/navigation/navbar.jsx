d067bec1f4f9d9774321978051861b6539f6b50c
samujpg <samu.a.huttunen@aalto.fi>
2026-04-20 11:49:29 +0300

    navbaria korjattu

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -5,14 +5,15 @@ function Navbar () {
   return (
     <nav className="navbar">
       <img src="/navlogo.png" alt="musta kirjekuori"></img>
-      <ul className="navbar-list">
-        <li className="navbar-item">
+      <div className="navbar-list">
+        <div className="navbar-item">
           <Link to="/about">About</Link>
-        </li>
-        <li className="navbar-item">
+        </div>
+        
+        <div className="navbar-item">
           <Link to="/">Home</Link>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   )
 
