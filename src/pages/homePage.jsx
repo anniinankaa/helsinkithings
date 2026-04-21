8613bc420e3a7949b57dc146ba7370fb16e04f08
samujpg <samu.a.huttunen@aalto.fi>
2026-04-21 16:10:44 +0300

    osa homepagen kuvista sommiteltu

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,8 +1,44 @@
+import './homePage.css'
+
 function HomePage () {
   return (
-    <div>
-      <h1>helsinkithings</h1>
-    </div>
+     <div className="homepics-container">
+       <div className="vert-container">
+
+        <div className="horz-container" id="pics1234567">
+
+          <div className='vert-container' id="pics1234">
+            <img src='/homefirstpics/pic1.png' alt='exhibition pic' id='pic1'></img>
+
+            <div className='horz-container' id='pics234'>
+              <img src='/homefirstpics/pic2.png' alt='street pic' id='pic2'></img>
+
+              <div className='vert-container' id='pics34'>
+                <img src='/homefirstpics/pic3.png' alt='shell pic' id='pic3'></img>
+                <img src='/homefirstpics/pic4.png' alt='pizza pic' id='pic4'></img>
+              </div>
+
+              <h1>helsinkithings</h1>
+            </div>
+          </div>
+
+          <div className='vert-container' id='pics567'>
+            <img src='/homefirstpics/pic5.png' alt='street pic' id='pic5'></img>
+            <img src='/homefirstpics/pic6.png' alt='event pic' id='pic6'></img>
+            <img src='/homefirstpics/pic7.png' alt='lake pic' id='pic7'></img>
+
+          </div>
+
+          <div className="horz-container">
+            <img src='/homefirstpics/pic8.png' alt='street pic' id='pic8'></img>
+            <img src='/homefirstpics/pic9.png' alt='event pic' id='pic9'></img>
+            <img src='/homefirstpics/pic10.png' alt='lake pic' id='pic10'></img>
+          </div> 
+        </div>  
+
+        
+       </div>
+     </div> 
   )
 }
 
