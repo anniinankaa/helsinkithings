ca1dd719b1cf39ae53a185727ae33470dba8c349
Vikke <vikke.savola@gmail.com>
2026-04-23 12:59:31 +0300

    about-sivun hero-osion edistys

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,22 @@
+import './about.css'
+
 function AboutPage () {
   return (
-    <div>
-      <h1>Hi! We're Helsinkithings</h1>
+    <div className="about-hero split-container">
+      <div className="left-panel">
+        <h1>A truly local guide sharing culture, places and stories that bring Helsinki to life.</h1>
+        <div className="socials">
+          <a href="" className="outline-button" style={{justifyContent: "flex-start"}}>
+            Instagram
+          </a>
+          <a href="" className="outline-button" style={{justifyContent: "flex-start"}}>
+            Tiktok
+          </a>
+        </div>
+      </div>
+      <div className="image-container">
+        <img src="/about/hero.png"></img>
+      </div>
     </div>
   )
 }
