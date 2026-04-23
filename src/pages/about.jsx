1a84cca2e0a91ded8a15bc20f5e37f389330c481
Vikke <vikke.savola@gmail.com>
2026-04-23 15:46:40 +0300

    about heron responsiivisuutta

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,18 +4,24 @@ function AboutPage () {
   return (
     <section className="about-hero split-container">
       <div className="left-panel">
-        <h1>A truly local guide sharing culture, places and stories that bring Helsinki to life.</h1>
-        <div className="socials">
-          <a href="" className="outline-button" style={{justifyContent: "flex-start"}}>
-            Instagram
-          </a>
-          <a href="" className="outline-button" style={{justifyContent: "flex-start"}}>
-            Tiktok
-          </a>
+        <div className="left-panel-top">
+          <h1>A truly local guide sharing culture, places and stories that bring Helsinki to life.</h1>
         </div>
-        <p>Helsinkithings offers cultural storytelling and community-driven marketing to create a lasting impact for your brand in the urban landscape.</p>
-        <br/>
-        <p>As your urban partner, we manage the entire lifecycle of a collaboration from scouting the unique narrative of your venue to producing high-engagement content.</p>
+        <div className="left-panel-bottom">
+          <div className="socials">
+            <a href="" className="outline-button" style={{justifyContent: "flex-start"}}>
+              Instagram
+            </a>
+            <a href="" className="outline-button" style={{justifyContent: "flex-start"}}>
+              Tiktok
+            </a>
+          </div>
+          <div>
+            <p>Helsinkithings offers cultural storytelling and community-driven marketing to create a lasting impact for your brand in the urban landscape.</p>
+            <br/>
+            <p>As your urban partner, we manage the entire lifecycle of a collaboration from scouting the unique narrative of your venue to producing high-engagement content.</p>
+          </div>
+        </div>
       </div>
       <div className="image-container">
         <img src="/about/hero.png" alt="a café with atmospheric lighting"></img>
