d8043721689cc2eabaa39bea4c667ad244db61fd
Vikke <vikke.savola@gmail.com>
2026-05-04 16:44:23 +0300

    paransin vähän testin käyttäjäkokemusta

--- src/components/ui/PicFrame.jsx
+++ src/components/ui/PicFrame.jsx
@@ -5,7 +5,7 @@ function PicFrame({lineColor, label, imageSrc, imageAlt, isActive, onClick}) {
 
   return (
     <div className={isActive ? styles.active : styles.picFrame} style={{"--border": lineColor}} onClick={onClick}>
-      <img src={imageSrc} alt={imageAlt}/>
+      <img src={imageSrc} alt={imageAlt} draggable={false}/>
       <h3>{label}</h3>
     </div>
   )
