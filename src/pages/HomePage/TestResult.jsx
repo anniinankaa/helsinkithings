6c0cbcebc363f9dccadddb373be6eda95a17094d
samujpg <samu.a.huttunen@aalto.fi>
2026-05-05 15:16:15 +0300

    nuolet lisätty

--- src/pages/HomePage/TestResult.jsx
+++ src/pages/HomePage/TestResult.jsx
@@ -1,27 +1,40 @@
 import './TestResult.css';
 import {events} from './Events.js';
 import SidewaysSection from '../../components/layout/SidewaysSection';
+import {useState} from 'react';
 
 function TestResult ({results}) {
     const cleaned = results.filter(s => s !== 'none');
 
     const options = events.filter(p => cleaned.every(k => p.tags.includes(k)));
 
+    const [currentIndex, setIndex] = useState(0);
+
+    const nextOption = () => {
+        setIndex((prev) => prev == options.length - 1 ? 0 : prev + 1); 
+    };
+
+    const prevOption = () => {
+        setIndex((prev) => prev == 0 ? options.length - 1 : prev - 1);
+    };
+
     return (
         <div className='result-screen'>
             <h2>Our suggestions based on your interests</h2>
             {options.length > 0 ? 
-            <div className='options-container'>{options.map(k => 
-                <div className='option'>
+            <div className='options-container'>{options.map((k, i) => 
+                <div className='option' style={currentIndex !== i ? {display: "none"}: null}>
                     <h3>{k.name}</h3>
                     <div className='text-box'>
                         <span>{k.description} <br/><br/> 
                         {k.info.address} <br/> 
                         <a href=''>{k.info.website} </a><br/> 
-                        <a href=''>{k.info.ighandle}</a> </span>
+                        <a href=''>{k.info.ighandle}</a></span>
                     </div>
                 </div>)}
-
+                <button className='right-button'onClick={nextOption}><img src='/arrows/rightarrow.png' alt='right arrow'/></button>
+                <button className='left-button'onClick={prevOption}><img src='/arrows/leftarrow.png' alt='left arrow'/></button>
+                
             </div>
             : <div className='option'>
                 <SidewaysSection header='No suggestions'/>
